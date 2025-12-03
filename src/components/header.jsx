2ec271b0fb5f36458a05e9f7d9b1630585a61001
Kavindu-Jayashan <mkkljayashan@gmail.com>
2025-12-03 21:24:44 +0530

    Refactor Header component to use buttons for navigation and enhance Homepage with a search bar and additional text

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,23 +3,23 @@ import { href, Navigate, useNavigate } from "react-router-dom";
 export default function Header() {
   const navigate = useNavigate();
   return (
-    <div className="w-full h-[100px] flex  ">
+    <div className="w-full h-[100px] flex z-96  ">
       <div className="w-[calc(60%)] h-[80px]  text-[28px] font-medium text-[#101010] flex justify-center items-center space-x-25 ">
-        <a href="/">Home</a>
-        <a href="/services">Services</a>
-        <a href="/wanted">Wanted</a>
-        <a href="/about">About</a>
-        <a href="/contact">Contact</a>
+        <button onClick={() => navigate("/")}>Home</button>
+        <button onClick={() => navigate("/services")}>Services</button>
+        <button onClick={() => navigate("/wanted")}>Wanted</button>
+        <button onClick={() => navigate("/about")}>About</button>
+        <button onClick={() => navigate("/contact")}>Contact</button>
       </div>
       <div className="w-[calc(40%)] h-[80px]  text-[28px] font-medium text-[#101010] flex justify-center items-center space-x-8 ">
-        <a href="/login">Login</a>
+        <button onClick={() => navigate("/login")}>Login</button>
         <button
-          // onClick={}
+          onClick={() => navigate("/signup")}
           className="w-[150px] h-[41px]    backdrop-blur-xl rounded-[20px] flex justify-center items-center text-[28px] font-medium text-[#101010]"
         >
           signup
         </button>
-        <button>theme</button>
+        <button onClick={() => console.log("theme clicked")}>theme</button>
       </div>
     </div>
   );
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -10,7 +10,24 @@ export default function Homepage() {
         </div>
         <div className="text-[#ffffff] text-[58px] font-semibold leading-[70px] pl-[112px] pt-[327px] absolute">
           Reliable Service,
-          <br /> Right at your Doorstep
+          <br />  Right at your Doorstep
+        </div>
+        <div className="text-[#ffffff] text-[34px] font-semibold leading-[70px] pl-[112px] pt-[510px] absolute">
+          Find Skilled Professionals Instantly
+        </div>
+        {/* Search bar  */}
+        <div className="w-[773px] h-[62px]   absolute leading-[70px]  pl-[112px] pt-[600px] flex">
+          <input
+            type="text"
+            name="search"
+            placeholder="Search For Services"
+            className="w-[calc(93%)] h-[50px] bg-amber-50 rounded-[10px] p-0 drop-shadow-[0_4px_4px_rgba(0,0,0,0.5)] pl-[20px] text-[24px] font-normal outline-none"
+            id=""
+          />
+          <button
+            onClick={() => console.log("search clicked")}
+            className="w-[calc(7%)] h-[50px] bg-[#FF5C00] pl-0 absolute  right-10 rounded-[10px] text-[#FFFFFF] text-[24px] font-semibold ml-4"
+          ></button>
         </div>
       </div>
       <div className="w-full h-full"></div>
