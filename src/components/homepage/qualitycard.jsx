940a816151e30e46d328c7ded5dc73eee485d12d
Kavindu-Jayashan <mkkljayashan@gmail.com>
2025-12-08 13:14:05 +0530

    Refactor Homepage component and update quality icons
    - Removed unnecessary whitespace in Homepage.jsx for cleaner code.
    - Updated the text in the main heading for consistency.
    - Replaced the search button's empty state with an image icon for better UX.
    - Changed quality icons to new images:
      - verified.png for "Verified & Trusted Technicians"
      - easybooking.png for "Fast & Easy Booking"
      - affordable.png for "Affordable Pricing"
      - range.png for "Wide Range of Services"
      - rating.png for "Ratings & Reviews"
      - contact.png for "Customer Support"
    - Removed unused footer whitespace for improved layout.

--- src/components/homepage/qualitycard.jsx
+++ src/components/homepage/qualitycard.jsx
@@ -1,8 +1,8 @@
 export default function QualityCard({ imgsrc, title, description }) {
   return (
-    <div className="w-[350px] h-[270px] bg-[#FFFFFF]  rounded-[10px] drop-shadow-[0_4px_4px_rgba(10,10,255,0.5)] ">
-      <div className="w-full h-[270px] flex flex-col justify-center items-center text-center">
-        <img className="w-[100px] h-[100px]" src={imgsrc} alt="" />
+    <div className="w-[350px] h-[270px] bg-[#FFFFFF]  rounded-[10px] drop-shadow-[0_4px_4px_rgba(10,110,255,0.5)] hover:scale-130 transition-all duration-300  ">
+      <div className="w-full h-[270px] flex flex-col justify-center items-center text-center ">
+        <img className="w-[120px] h-[125px]" src={imgsrc} alt="" />
         <span className="text-[24px] font-semibold pt-4 text-[#4B9EBE]">{title}</span>
         <span className="text-[20px] font-serif pt-4">{description}</span>
       </div>
