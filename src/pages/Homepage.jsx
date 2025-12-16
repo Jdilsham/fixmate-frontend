a035d9f8f08f46438c7aa91d534e7beee0753279
Kavindu-Jayashan <mkkljayashan@gmail.com>
2025-12-16 23:49:55 +0530

    fix: adjust footer height and visibility of quick/popular links for improved layout

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -154,7 +154,7 @@ export default function Homepage() {
         </div>
       </div>
       {/* fourth quarter - footer */}
-      <div className="w-full h-[800px] bg-[url('/footer.png')] bg-cover bg-bottom flex justify-center items-center">
+      <div className="w-full h-[500px] bg-cover bg-bottom flex justify-center items-center">
         <Footercard />
       </div>
     </div>
