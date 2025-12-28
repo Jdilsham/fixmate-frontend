168a220e7eac02a62c328d94569ac9ac10dd60cc
sewwandi300 <kmmsfernando2@gmail.com>
2025-12-28 20:42:44 +0530

    Add Contact page

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,31 +1,62 @@
-import Footercard from "../components/footer";
 import Header from "../components/header";
+import Footercard from "../components/footer";
+import ContactCard from "../components/contactpage/contactCard";
 
+export default function About() {
+  return (
+    <div className="w-full min-h-screen bg-background text-foreground transition-colors">
+      <Header />
+     
+      <section className="relative max-w-7xl mx-auto px-6 pt-28 pb-22">
+         <h1 className="text-6xl md:text-7xl font-semibold leading-tight max-w-4xl">
+             Contact
+           <span className="text-accent">   Us</span>
+         </h1>
 
-export default function Contact(){
-    return(
-        <div className="w-full h-full">
-            {/* first quarter */}
-            <Header />
-            <div className="w-full bg-[url('/contact-background.jpg')] bg-center bg-cover h-screen bg-no-repeat">
-             <div className="absolute left-[270px] top-[210px] font-inter text-[90px] font-normal leading-none">
-                <span className="text-white">Contact </span>
-                <span className="text-[#01BDEF]">Us</span>
-             </div>
-            
-              <div className="absolute left-[305px] top-[340px] font-inter text-[32px] font-normal leading-none">
-                <span className="text-white">Fixmate is ready to provide the<br/> right solution according to<br/>your needs  </span>
-              </div>
+         <p className="mt-8 text-xl text-muted-foreground max-w-xl">
+          Fixmate is ready to provide the right solution according <br/>to your needs.
+         </p>
 
-             <img src="/tel.png" className="absolute left-[130px] top-[270px] w-[510px] h-[510px] object-contain"/>
-            </div>
+      </section>
 
+         {/* ==============Contact & Join Together============== */}
+         <div className="max-w-7xl mx-auto px-6 pb-36">
+            <h2 className="text-[48px] font-serif text-center mb-12">
+                Contact & Join Together
+            </h2>
 
-
-
-
-            <Footercard/>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10">
+          <ContactCard
+            icon="/contactIcons/location.png"
+            title="Address"
+            value="Galle Road, Matara"
+          />
+          <ContactCard
+            icon="/contactIcons/phone.png"
+            title="Phone"
+            value="+94 33 265 394"
+          />
+          <ContactCard
+            icon="/contactIcons/whatsapp.png"
+            title="Whatsapp"
+            value="+94 74 258 889"
+          />
+          <ContactCard
+            icon="/contactIcons/email.png"
+            title="Email"
+            value="support@fixmate.lk"
+          />
         </div>
-            
-    );
+      </div>
+
+      
+
+
+
+    {/* ================= FOOTER ================= */}
+      <section className="bg-background pt-32 pb-24">
+         <Footercard />
+      </section>
+    </div>
+  );
 }
