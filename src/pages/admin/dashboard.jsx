e63485caae9683913a6199017faabb73c7a04bde
Kavindu-Jayashan <mkkljayashan@gmail.com>
2026-01-18 12:56:57 +0530

    feat: refactor admin dashboard components and implement tab navigation for improved user experience

--- src/pages/admin/dashboard.jsx
+++ src/pages/admin/dashboard.jsx
@@ -3,8 +3,26 @@ import Header from "@/components/header";
 import AdminSidebar from "../../components/adminDashboard/sidebar";
 import AdminStats from "../../components/adminDashboard/adminStat";
 import PendingProvidersTable from "../../components/adminDashboard/pendingProviderTable";
+import { useState } from "react";
+import {
+  Briefcase,
+  FileCheck,
+  LayoutDashboard,
+  Settings,
+  Users,
+} from "lucide-react";
+
+const tabs = [
+  { id: "dashboard", label: "Dashboard", icon: LayoutDashboard },
+  { id: "approval", label: "Provider Approvals", icon: FileCheck },
+  { id: "users", label: "Users", icon: Users },
+  { id: "services", label: "Services", icon: Briefcase },
+  { id: "settings", label: "Settings", icon: Settings },
+];
 
 export default function AdminDashboard() {
+  const [activeTab, setActiveTab] = useState("dashboard");
+
   return (
     <div className="flex min-h-screen bg-background">
       <AdminSidebar />
@@ -13,11 +31,28 @@ export default function AdminDashboard() {
         <Header />
 
         <main className="p-6 space-y-6">
-          <h1 className="text-2xl font-semibold">Admin Dashboard</h1>
+          <div className=" border rounded-2xl p-6 flex flex-col justify-around ">
+            {/* DashBoard */}
+            {activeTab === "dashboard" && (
+              <>
+              <div className="flex flex-col gap-8 ">
+                <AdminStats />
+              <PendingProvidersTable />
+              </div>
+              </>
+            )
+              
+            }
 
-          <AdminStats />
+            {/* Approvals */}
+            {activeTab === "approval" && (
+              <>
+              
+              </>
+            )}
+          </div>
 
-          <PendingProvidersTable />
+         
         </main>
       </div>
     </div>
